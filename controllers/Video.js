59bafdebac979019cdf72c9eb9530c56be89634e
Jenel Myers <jenelmyers@Jenels-MacBook-Pro.local>
2023-06-12 19:37:43 -0400

    Add description

--- controllers/Video.js
+++ controllers/Video.js
@@ -4,10 +4,8 @@ exports.compressVideo = (videoPath, outputPathh, callback) => {
   // Input video file path
   // Output video file path
   const outputPath = `tmp/compressed_${outputPathh}.mp4`
-
-  // Define the desired bitrate for compression (e.g., 1/5th of the original bitrate)
   const originalBitrate = 1000 // Original bitrate in Kbps
-  const desiredBitrate = originalBitrate / 5 // Desired bitrate in Kbps
+  const desiredBitrate = originalBitrate / 4 // Desired bitrate in Kbps
   // Compress the video
   console.log(videoPath.tempFilePath)
   ffmpeg(videoPath.tempFilePath)
